14cf3876f0b464c286787b8ec6ff103b0b85c2f6
Maimas2 <aseltz156@gmail.com>
2025-08-20 19:36:30 -0400

    Try change to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,14 @@ plugins {
 android {
     namespace = "com.alexseltzer.uscodereader"
     compileSdk = 36
+    
+    applicationId = "com.alexseltzer.uscodereader"
+    minSdk = 24
+    targetSdk = 36
+    versionCode = 1
+    versionName = "1.0"
 
     defaultConfig {
-        applicationId = "com.alexseltzer.uscodereader"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
